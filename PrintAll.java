79f7e3697869592216ba24b6ee4564ed7eaaeb8e
azmuhamm <59297518+azmuhamm@users.noreply.github.com>
2023-02-10 15:45:43 -0500

    Add files via upload
    Java program to print position, direction, and orientation vectors from different parts of arm.

---
+++ PrintAll.java
@@ -0,0 +1,101 @@
+import java.io.IOException;
+import java.lang.Math;
+import com.leapmotion.leap.*;
+
+class SampleListener1 extends Listener {
+    public void onInit(Controller controller) {
+        System.out.println("Initialized");
+    }
+
+    public void onConnect(Controller controller) {
+        System.out.println("Connected");
+    }
+
+    public void onDisconnect(Controller controller) {
+        //Note: not dispatched when running in a debugger.
+        System.out.println("Disconnected");
+    }
+
+    public void onExit(Controller controller) {
+        System.out.println("Exited");
+    }
+
+    public void onFrame(Controller controller) {
+        // Get the most recent frame and report some basic information
+        Frame frame = controller.frame();
+        System.out.println("Frame id: " + frame.id()
+                         + ", timestamp: " + frame.timestamp()
+                         + ", hands: " + frame.hands().count()
+                         + ", fingers: " + frame.fingers().count());
+
+        //Get hands
+        for(Hand hand : frame.hands()) {
+            String handType = hand.isLeft() ? "Left hand" : "Right hand";
+            System.out.println("  " + handType + ", id: " + hand.id()
+                             + ", palm position: " + hand.palmPosition());
+
+            // Get the hand's normal vector and direction
+            Vector normal = hand.palmNormal();
+            Vector direction = hand.direction();
+
+            // Calculate the hand's pitch, roll, and yaw angles
+            System.out.println("  pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
+                             + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
+                             + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees");
+
+            // Get arm bone
+            Arm arm = hand.arm();
+            System.out.println("  Arm direction: " + arm.direction()
+                             + ", wrist position: " + arm.wristPosition()
+                             + ", elbow position: " + arm.elbowPosition());
+
+            // Get fingers
+            for (Finger finger : hand.fingers()) {
+                System.out.println("    " + finger.type() + ", id: " + finger.id()
+                                 + ", length: " + finger.length()
+                                 + "mm, width: " + finger.width() + "mm");
+
+                //Get Bones
+                for(Bone.Type boneType : Bone.Type.values()) {
+                    Bone bone = finger.bone(boneType);
+                    System.out.println("      " + bone.type()
+                                     + " bone, start: " + bone.prevJoint()
+                                     + ", end: " + bone.nextJoint()
+                                     + ", direction: " + bone.direction());
+                }
+            }
+        }
+        // Sleep to allow for easier readability of output
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        if (!frame.hands().isEmpty()) {
+            System.out.println();
+        }
+    }
+}
+
+class Sample1 {
+    public static void main(String[] args) {
+        // Create a sample listener and controller
+        SampleListener1 listener = new SampleListener1();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
+    }
+}
