e4fd1a7889fc997971a569efa465844aa68e1e82
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2023-03-02 22:00:26 -0500

    Slow servo java changes

--- hwdemo/src/CRCTesting.java
+++ hwdemo/src/CRCTesting.java
@@ -15,66 +15,102 @@ public class CRCTesting {
     private static final int BAUD_RATE = 115200;
     
     public static void main(String[] args) throws Exception {
-            /*
-            String bits = "";
-            for (int i=0; i<positions.length; i++) {
-                bits += Integer.toBinaryString(positions[i]);
-            }
-            System.out.println("raw converted: " + bits);
-            int check1 = crc8(bits);
-            System.out.println("CRC: "+check1);
-            String crc = Integer.toBinaryString(check1);
-            System.out.println("CRC: "+crc);
-            String message = bits + crc;
-            System.out.println("Message: "+message);
-            int check2 = crc8(message.substring(0, message.length() - 7));
-            System.out.println("Checked value: "+check2);
-            */
-            /*
-            byte[] packetArray = {0,25,50,75,110,125,0,0,120,70};
-            CRCPacket p = new CRCPacket(packetArray);
-            byte[] msg = p.compile();
-            for(byte b : msg) {
-                System.out.print((b & 0xFF)+", ");
-            }
-            */
-            /*
-            System.out.println((byte)250);
-            byte[] packetArray = {0,25,50,75,110,125,0,0,120,70};
-            CRCPacket p = new CRCPacket(packetArray);
-            System.out.println("CRC: "+p.computeCRC());
-            */
-            //byte abyte = (byte)170;
-            //int aint = Byte.toUnsignedInt(abyte);
-       
-            byte[] packetArray = {0,25,50,75,110,125,0,0,120,(byte)170};
+            byte[] packetArray = {90,97,45,90,(byte)180,(byte)180,(byte)180,(byte)180,(byte)180,(byte)180};
+            byte[] packetArray1 = {90,97,0,90,0,(byte)0,(byte)0,(byte)0,(byte)0,(byte)0};
             //byte[] packetArray = {(byte)240,(byte)240,(byte)240,(byte)240,(byte)240,(byte)240,(byte)240,(byte)240,(byte)240,(byte)240};
-            CRCPacket p = new CRCPacket(packetArray);
+            Packet p = new Packet(packetArray);
+            Packet p1 = new Packet(packetArray1);
             Clock clock = Clock.systemDefaultZone();
             Arduino a = new Arduino(Arduino.detectArduino());
             a.open(BAUD_RATE);
             int c = 0;
             
             long startTime = clock.millis();
-            while (c < 5000) {
-                a.writePacket(p);
+            /*
+            a.writePacket(p);
+            System.out.println("wrote packet " + c + ". listening..");
+            byte rpacket[] = a.listenForAndReadPacket();
+            if (rpacket[0] == Packet.BADCRC) {
+                System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+            }
+            System.out.println("got packet back: ");
+            for (byte b : rpacket) {
+                System.out.print((b & 0xFF) + ", ");
+            }
+            System.out.println();
+            
+            a.writePacket(p1);
+            System.out.println("wrote packet " + c + ". listening..");
+            rpacket = a.listenForAndReadPacket();
+            if (rpacket[0] == Packet.BADCRC) {
+                System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+            }
+            System.out.println("got packet back: ");
+            for (byte b : rpacket) {
+                System.out.print((b & 0xFF) + ", ");
+            }
+            System.out.println();
+            
+            a.writePacket(p);
+            System.out.println("wrote packet " + c + ". listening..");
+            rpacket = a.listenForAndReadPacket();
+            if (rpacket[0] == Packet.BADCRC) {
+                System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+            }
+            System.out.println("got packet back: ");
+            for (byte b : rpacket) {
+                System.out.print((b & 0xFF) + ", ");
+            }
+            System.out.println();
+            
+            a.writePacket(p1);
+            System.out.println("wrote packet " + c + ". listening..");
+            rpacket = a.listenForAndReadPacket();
+            if (rpacket[0] == Packet.BADCRC) {
+                System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+            }
+            System.out.println("got packet back: ");
+            for (byte b : rpacket) {
+                System.out.print((b & 0xFF) + ", ");
+            }
+            System.out.println();
+            */
+            
+            while (true) {
                 c++;
-                System.out.println("wrote packet " + c + ". listening..");
-                byte rpacket[] = a.listenForAndReadPacket();
-                if (rpacket[0] == CRCPacket.BADCRC) {
-                    System.out.println("Received Bad PAcket (CRC ERROS ON LINE)");
+                if ((c % 2) == 0) {
+                    a.writePacket(p);
+                    System.out.println("wrote packet " + c + ". listening..");
+                    byte rpacket[] = a.listenForAndReadPacket();
+                    if (rpacket[0] == Packet.BADCRC) {
+                        System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+                    }
+                    System.out.println("got packet back: ");
+                    for (byte b : rpacket) {
+                        System.out.print((b & 0xFF) + ", ");
+                    }
+                    System.out.println();
                 }
-                System.out.println("got packet back: ");
-                for (byte b : rpacket) {
-                    System.out.print((b & 0xFF) + ", ");
-                }
-                System.out.println();
+                else {
+                    a.writePacket(p1);
+                    System.out.println("wrote packet " + c + ". listening..");
+                    byte rpacket[] = a.listenForAndReadPacket();
+                    if (rpacket[0] == Packet.BADCRC) {
+                        System.out.println("Received Bad PAcket (CRC ERRORS ON LINE)");
+                    }
+                    System.out.println("got packet back: ");
+                    for (byte b : rpacket) {
+                        System.out.print((b & 0xFF) + ", ");
+                    }
+                    System.out.println();
+                    }
                 //Thread.sleep(1);
 
                 }
-            long endTime = clock.millis();
-            long diff = (endTime - startTime)/5000;
-            System.out.println("Packet ms: " + diff);
+                
+            //long endTime = clock.millis();
+            //long diff = (endTime - startTime)/5000;
+            //System.out.println("Packet ms: " + diff);
 
     }
 }
