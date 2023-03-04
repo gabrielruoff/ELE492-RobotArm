87817bd781e89d3d379057969036e48ac57b5c18
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2023-03-04 13:46:03 -0500

    Cleaning (Java)

--- hwdemo/src/Arduino.java
+++ hwdemo/src/Arduino.java
@@ -8,8 +8,10 @@ public class Arduino {
 	private static final String OSX_PORT_PATH = "/dev/cu.usbmodem11301";
 	public SerialPort serialPort;
 	public InputStream inputStream;
+	public Packet oldPacket;
 
 	public Arduino(SerialPort serialPort) {
+		this.oldPacket = new Packet(new byte[] {90,97,90,90,90,(byte)180,(byte)180,(byte)180,(byte)180,(byte)180});
 		this.serialPort = serialPort;
 	}
 
@@ -33,43 +35,98 @@ public class Arduino {
 	}
 
 	public void write(byte[] message) {
-		serialPort.writeBytes(message, message.length);
+                serialPort.writeBytes(message, message.length);
 	}
 
+	@Deprecated
 	public void writePacket(Packet packet) {
 		this.write(packet.compile());
 	}
-
-	public void waitForPacketStart() throws IOException {
-		while (true) {
-			if (inputStream.available() == 0)
-				continue;
-			if (inputStream.read() != Packet.START)
-				return;
+	
+	public void writePacketVarSpeed(Packet packet, int steps, int delay) throws Exception {
+		int deltas[] = new int[Packet.PACKET_LENGTH];
+		for(int i=0;i<deltas.length;i++)
+		{
+			deltas[i] = (packet.positions[i]-oldPacket.positions[i])/steps;
 		}
+		for(int i=1;i<=steps;i++)
+		{
+			for(int j=0;j<Packet.PACKET_LENGTH;j++)
+			{
+				packet.positions[i] = (byte)(oldPacket.positions[i]+(i*deltas[j]));
+			}
+			System.out.println("packet: "+packet.toString());
+			this.write(packet.compile());
+			System.out.println("wrote packet. listening..");
+			byte rpacket[] = this.listenForAndReadPacket();
+			System.out.println("got packet back: ");
+			for (byte b : rpacket) {
+				System.out.print((b & 0xFF) + ", ");
+			}
+			System.out.println();
+			Thread.sleep(delay);
+		}
+		oldPacket = packet;
+//		this.write(packet.compile());
 	}
 
-	public boolean checkForPacketStop() throws IOException {
-		while (inputStream.available() == 0)
-			;
-		return inputStream.read() == Packet.STOP;
+	public void waitForPacketStart() throws IOException {
+            while (true) {
+		if (inputStream.available() > 0 && (byte)inputStream.read() == Packet.START) {
+                    return;
+                }	
+            }
+	}
+
+	public void waitForPacketStop() throws Exception {
+            int c = 0;
+            while (true) {
+		c++;
+                if (inputStream.available() > 0 && (byte)inputStream.read() == Packet.STOP) {
+                    break;
+                }
+                if (c > 1000) {
+                    throw new Exception("Expected packed stop");
+                }
+            }
 	}
 
 	public byte[] digestPacket() throws IOException {
-		byte packet[] = new byte[Packet.PACKET_LENGTH];
-		while (inputStream.available() < Packet.PACKET_LENGTH);
-		for (int i = 0; i < Packet.PACKET_LENGTH; i++) {
-			packet[i] = (byte) inputStream.read();
-		}
-		return packet;
+		byte inputPackets[] = new byte[Packet.PACKET_LENGTH+1];
+                for (int i = 0; i < Packet.PACKET_LENGTH+1; i++) {
+                    while(true) {
+                        if (inputStream.available() > 0) {
+                            inputPackets[i] = (byte)inputStream.read();
+                            break;
+                        }
+                    }
+                }
+		return inputPackets;
+	}
+        
+        public boolean validateCRC(byte inputPackets[]) throws IOException {
+		byte readCRC = inputPackets[10];
+                byte packetData[] = new byte[10];
+                System.arraycopy(inputPackets, 0, packetData, 0, 10);
+                Packet crosscheck  = new Packet(packetData);
+                crosscheck.setCRC();
+                byte calcCRC = crosscheck.getCRC();
+                System.out.println("Got CRC: "+readCRC+" Calced CRC: "+calcCRC);
+                return readCRC == calcCRC;
 	}
 
 	public byte[] listenForAndReadPacket() throws Exception {
-		this.waitForPacketStart();
-		byte packet[] = this.digestPacket();
-		if (!this.checkForPacketStop())
-			throw new Exception("Expected packed stop");
-		return packet;
+                this.waitForPacketStart();
+		byte inputPackets[] = this.digestPacket();
+                this.waitForPacketStop();
+                if (validateCRC(inputPackets)) {
+                    //System.out.println("CRC GOOD");
+                }
+                else {
+//                    System.out.println("CRC BAD");
+                    throw new Exception("BAD CRC");
+                }
+		return inputPackets;
 	}
 
 }
---
+++ hwdemo/src/VarSpeedTest.java
@@ -0,0 +1,17 @@
+import java.time.Clock;
+
+public class VarSpeedTest {
+	private static final int BAUD_RATE = 115200;
+	
+	public static void main(String[] args) throws Exception
+	{
+		Packet neutral = new Packet(new byte[] { 90, 97, 90, 90, 90, (byte) 180, (byte) 180, (byte) 180, (byte) 180, (byte) 180 });
+		Clock clock = Clock.systemDefaultZone();
+		Arduino a = new Arduino(Arduino.detectArduino());
+		a.open(BAUD_RATE);
+		
+		neutral.setWrist(0);
+		a.writePacketVarSpeed(neutral, 10, 100);
+
+	}
+}
