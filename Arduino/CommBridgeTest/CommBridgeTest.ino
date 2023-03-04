3d537d3d4bc103289d8d1a6b9695589eedbb2f13
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2023-03-04 13:43:59 -0500

    Cleaning

--- Arduino/CommBridgeTest/CommBridgeTest.ino
+++ Arduino/CommBridgeTest/CommBridgeTest.ino
@@ -6,23 +6,15 @@ RobotArm arm = RobotArm(BAUD_RATE);
 int* packet;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(BAUD_RATE);
   arm.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  arm.waitForPacket();
+  //Read Next Packet
   packet = arm.readPacket();
-  arm.waitForPacketEnd();
-//  Serial.print("Received packet: ");
-//  for(int i=0;i<arm.PACKET_LENGTH;i++)
-//  {
-//    Serial.write(packet[i]);
-//  }
-//  Serial.println();
-  arm.updateFromPacket(packet);
+  //Update Servo Positions Internally
+  arm.updateFromPacket();
+  //Send Packet Back
   arm.sendPacket(packet);
-//   delay(100);
 }
