ecec6754a262c94248124b862164d3ce812b04a2
SubToxic101 <86693701+SubToxic101@users.noreply.github.com>
2023-03-06 18:49:25 -0500

    Java Changes

--- hwdemo/src/VarSpeedTest.java
+++ hwdemo/src/VarSpeedTest.java
@@ -5,25 +5,68 @@ public class VarSpeedTest {
 	
 	public static void main(String[] args) throws Exception
 	{
-		Packet neutral = new Packet(new byte[] { 90, 97, 90, 90, 90, (byte) 180, (byte) 180, (byte) 180, (byte) 180, (byte) 180 });
+		Packet neutral = new Packet(new byte[] {90, 97, 0, (byte)135, 90, (byte) 180, (byte) 180, (byte) 180, (byte) 180, (byte) 180 });
 		Clock clock = Clock.systemDefaultZone();
 		Arduino a = new Arduino(Arduino.detectArduino());
 		a.open(BAUD_RATE);
 		
+		// send default
+		a.writePacketVarSpeed(neutral, 60, 20);
+                
+                System.out.println("press enter");
+                System.in.read();
+                
+                // rotate wrist
+                neutral.setWristRotation(0);
+                neutral.setShoulderRotation(145);
+                a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setWristRotation(180);
+                neutral.setShoulderRotation(90);
+                a.writePacketVarSpeed(neutral, 60, 20);
+                
+                System.out.println("press enter");
+                System.in.read();
 		
-		a.writePacketVarSpeed(neutral, 10, 100);
+		// move shoulder
+		neutral.setShoulder(160);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setShoulder(50);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setShoulder(97);
+		a.writePacketVarSpeed(neutral, 60, 20);
 		
-		// move wrist
-		neutral.setWrist(0);
-		a.writePacketVarSpeed(neutral, 10, 100);
-//		
-//		neutral.setShoulder(103);
-		neutral.setWrist(180);
-		a.writePacketVarSpeed(neutral, 20, 0);
-//		neutral.setWrist(90);
-//		a.writePacketVarSpeed(neutral, 10,100);
-		
-		
+                System.out.println("press enter");
+                System.in.read();
+                
+                // move elbow
+		neutral.setElbow(180);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setElbow(90);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                //move wrist
+                neutral.setWrist(0);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setWrist(180);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                neutral.setWrist(135);
+		a.writePacketVarSpeed(neutral, 60, 20);
+                //move elbow back to 0
+                neutral.setElbow(0);
+		a.writePacketVarSpeed(neutral, 60, 20);
 
+                System.out.println("press enter");
+                System.in.read();
+                for(int i=1;i<6; i++)
+                    neutral.setFinger(i, 0);
+                a.writePacketVarSpeed(neutral, 60,25);
+                for(int i=1;i<6; i++)
+                    neutral.setFinger(i, 180);
+                a.writePacketVarSpeed(neutral, 60,25);
+                for(int i=1;i<6; i++)
+                    neutral.setFinger(i, 0);
+                a.writePacketVarSpeed(neutral, 60,25);
+                for(int i=1;i<6; i++)
+                    neutral.setFinger(i, 180);
+                a.writePacketVarSpeed(neutral, 60,25);
 	}
 }
