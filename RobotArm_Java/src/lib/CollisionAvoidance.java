035245a6b00f67b03cfbc895e7633c19ffe67d3f
Gabriel Ruoff <geruoff@gmail.com>
2023-04-19 23:56:29 -0400

    Collision avoidance check for claw vs bicep

--- RobotArm_Java/src/lib/CollisionAvoidance.java
+++ RobotArm_Java/src/lib/CollisionAvoidance.java
@@ -1,15 +1,17 @@
 package lib;
 
+import java.util.Vector;
+
 import org.ejml.simple.SimpleMatrix;
 
 public class CollisionAvoidance {
 	// millimeters
-	public static final int PADDING = 0;
+	public static final int PADDING = -50;
 	public static final int bicepR = 30;
 	public static final int bicepL = 300;
 	public static final int forearmL = 250;
 	public static final int wristL = 80;
-	public static final int clawL = 100;
+	public static final int clawL = 80;
 	public static boolean validatePosition(TransformedPose p) {
 		byte pos[] = new byte[p.positions.length];
 		for(int i=0;i<p.positions.length;i++) {
@@ -20,9 +22,9 @@ public class CollisionAvoidance {
 	
 	public static boolean validatePosition(byte positions[]) {
 		// normalize
-		float elbowNorm = Arduino.byteToInt(positions[TransformedPose.elbow])-90;
+		float elbowNorm = Arduino.byteToInt(positions[TransformedPose.elbow])-150;
 		float wristNorm = Arduino.byteToInt(positions[TransformedPose.wrist])-90;
-		
+		System.out.println(elbowNorm);
 		SimpleMatrix shoulderR = createRMatrix(positions[TransformedPose.shoulder]);
 		SimpleMatrix shoulderT = createTMatrix(bicepL, 0);
 		SimpleMatrix elbowR = createRMatrix(elbowNorm);
@@ -32,9 +34,39 @@ public class CollisionAvoidance {
 //		System.out.println(shoulderR.toString());
 //		System.out.println(shoulderT.toString());
 //		System.out.println(wristR.toString());
-		SimpleMatrix clawPosition = shoulderR.mult(shoulderT).mult(elbowR).mult(elbowT)
-				.mult(wristR).mult(wristT);
-//		System.out.println(clawPosition.toString());
+		System.out.println(shoulderR.mult(shoulderT).toString());
+		SimpleMatrix elbowTipPosition = shoulderR.mult(shoulderT).mult(elbowR).mult(elbowT);
+		System.out.println(elbowTipPosition.toString());
+		SimpleMatrix clawPosition = elbowTipPosition.mult(wristR).mult(wristT);
+		System.out.println(clawPosition.toString());
+		return tableCollisionCheck(elbowTipPosition) && tableCollisionCheck(clawPosition) && limbCollisionCheck(clawPosition, shoulderR);
+	}
+	
+	private static boolean limbCollisionCheck(SimpleMatrix clawPosition, SimpleMatrix shoulderR) {
+		SimpleMatrix rotateMinus90 = createRMatrix(-90);
+		SimpleMatrix bicepW = createTMatrix(bicepR, 0);
+		// point A
+		SimpleMatrix edgePoint = shoulderR.mult(rotateMinus90).mult(bicepW);
+		double edgeLine[] = new double[] {edgePoint.get(0, 2), edgePoint.get(1,2), Math.tan(Math.acos(shoulderR.get(0, 0)))};
+		System.out.println("Parallel line at (Y-"+edgeLine[1]+") = "+edgeLine[2]+"*(X-"+edgeLine[0]);
+		// point B
+		double XAtYIsZero = xPointFromLineEq(edgeLine, 0);
+		// determinant of vectors AB (between both line points) and AQ (between point A and query point)
+		double determinant = (XAtYIsZero - edgeLine[0]) * (clawPosition.get(1, 2) - edgeLine[1]) - (0 - edgeLine[1]) * (clawPosition.get(0, 2) - edgeLine[0]);
+		System.out.println("Point A ("+edgePoint.get(0, 2)+","+edgePoint.get(1,2)+") Point B ("+XAtYIsZero+",0)");
+		System.out.println("determinant "+determinant);
+		Boolean valid = true;
+		if(determinant > 0) {
+			valid = clawPosition.get(1,2)>bicepL*Math.sin(Math.acos(shoulderR.get(0, 0)));
+		}
+		return valid;
+	}
+	
+	private static double xPointFromLineEq(double line[], double y) {
+		return (y-line[1])/(line[2])+line[0];
+	}
+	
+	private static boolean tableCollisionCheck(SimpleMatrix clawPosition) {
 		return clawPosition.get(1,2)>PADDING;
 	}
 	
